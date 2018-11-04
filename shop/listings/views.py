7dfb88e77186fd2bd5fb176e82920546265ddc91
fr3kz <tektmap1@gmail.com>
2018-11-04 16:05:51 +0200

    shop project

--- shop/listings/views.py
+++ shop/listings/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 # Create your views here.
 def index(request):
@@ -15,8 +15,11 @@ def index(request):
     return render(request,'listings/listings.html',contex)
 
 def listing(request,listing_id):
-    # TODO: create a signle page view
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing,pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html',context)
 
 def search(request):
     return render(request, 'listings/search.html')
--- shop/pages/views.py
+++ shop/pages/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 def index(request):
-    return render(request,"pages/index.html")
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    contex = {
+        'listings': listings
+    }
+
+    return render(request,"pages/index.html", contex)
 
 def about(request):
-    return render(request,"pages/about.html")
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)[:1]
+    context = {
+        'realtors': realtors,
+        'mvp_realtor': mvp_realtors
+    }
+    return render(request,"pages/about.html",context)
