575de213e1a797fb34dee743a9c4dd41a643bab9
fr3kz <tektmap1@gmail.com>
2018-11-12 17:35:41 +0200

    shop project

--- shop/pages/views.py
+++ shop/pages/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices,bedroom_choices,state_choices
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
-    contex = {
-        'listings': listings
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
 
-    return render(request,"pages/index.html", contex)
+    return render(request, "pages/index.html", context)
+
 
 def about(request):
     realtors = Realtor.objects.order_by('-hire_date')
@@ -18,4 +23,4 @@ def about(request):
         'realtors': realtors,
         'mvp_realtor': mvp_realtors
     }
-    return render(request,"pages/about.html",context)
+    return render(request, "pages/about.html", context)
