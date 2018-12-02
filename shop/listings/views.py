f3d76c08246a55493f4a78bde5997447148cce1e
fr3kz <tektmap1@gmail.com>
2018-12-02 11:53:25 +0200

    test_commit

--- shop/listings/views.py
+++ shop/listings/views.py
@@ -1,25 +1,72 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+
+from .choices import bedroom_choices, price_choices, state_choices
 from .models import Listing
+
+
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
-    paginator = Paginator(listings,1)
+    paginator = Paginator(listings, 1)
     page = request.GET.get('page')
     paged_list = paginator.get_page(page)
 
     contex = {
-        'listings':paged_list
+        'listings': paged_list
     }
 
-    return render(request,'listings/listings.html',contex)
+    return render(request, 'listings/listings.html', contex)
 
-def listing(request,listing_id):
-    listing = get_object_or_404(Listing,pk=listing_id)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
     context = {
         'listing': listing
     }
-    return render(request, 'listings/listing.html',context)
+    return render(request, 'listings/listing.html', context)
+
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(description__icontains=keywords)
+
+    # city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list = query_list.filter(city__iexact=city)
+
+    # state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list = query_list.filter(state__iexact=state)
+
+     # price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list = query_list.filter(price__lte=price)   
+
+
+     # bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list = query_list.filter(bedrooms__lte=bedrooms)                                 
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': query_list,
+        'values': request.GET,
+    }
+
+    return render(request, 'listings/search.html', context)
